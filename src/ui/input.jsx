1dd930ea9c33e6e5d6f9ec6d01be890d581d8871
AdeMo <58342559+AdemoDEV@users.noreply.github.com>
2025-06-21 11:48:14 +0200

    Delete fichier

--- src/ui/input.jsx
+++
@@ -1,5 +0,0 @@
-export function Input({ className = '', ...props }) {
-  return (
-    <input className={`border border-gray-300 rounded-xl px-3 py-2 w-full ${className}`} {...props} />
-  );
-}
--- src/ui/textarea.jsx
+++
@@ -1,5 +0,0 @@
-export function Textarea({ className = '', ...props }) {
-  return (
-    <textarea className={`border border-gray-300 rounded-xl px-3 py-2 w-full ${className}`} {...props} />
-  );
-}
